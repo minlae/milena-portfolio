91725ae771c1d268832d7ec8254391bd78b3fc4b
Milena Djokic <milena.djokic@gmail.com>
2017-09-20 00:07:30 -0400

    fixed up portfolio images

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
-	$('#nav-icon3').click(function(){
-		$(this).toggleClass('open');
+	$("#nav-icon3").click(function(){
+		console.log(this);
+		$(this).toggleClass("open");
 		const smallMenu = $(".smallMenu");
+		const test = this;
 
 		smallMenu.slideToggle(300);		
 		
+		$("#aboutLink").click(function() {
+			// console.log(test);	
+			// HELP: This works the FIRST time you click "About" but then afterward it does something weird.
+			$(test).toggleClass("open");
+			smallMenu.slideToggle(300);	
+		})
 	});
+
 
 	function goToByScroll(id) {
 		let toMe = $("." + id)
@@ -16,8 +25,24 @@ $(document).ready(function(){
 		}, 500);
 	};
 
-	$('#mainButton').on('click', function() {
+	$("#mainButton").on("click", function() {
 		goToByScroll("portfolio");
-	})
+	});
+
+	$("#navSkills").on("click", function() {
+		goToByScroll("skills");
+	});
+
+	$("#navPortfolio").on("click", function() {
+		goToByScroll("portfolio");
+	});
+
+	$("#navPortfolio").on("click", function() {
+		goToByScroll("portfolio");
+	});
+
+	$("#navContact").on("click", function() {
+		goToByScroll("contact");
+	});
 
 });
