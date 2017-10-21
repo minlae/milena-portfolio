513a30fdaac8f2b40cade42811156fca7a364966
Milena Djokic <milena.djokic@gmail.com>
2017-10-21 12:02:58 -0400

    organized js code

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,19 +1,28 @@
-$(document).ready(function(){
+const milenaSite = {}
+
+milenaSite.init = () => {
+	milenaSite.hamburgerMenu();
+	milenaSite.scrolling();
+};
+
+milenaSite.hamburgerMenu = () => {
+	// Hamburger menu in mobile version
+	// When you click on the hamburger icon ("nav-icon3"), opens or closes the menu - with slide motion.
 	$("#nav-icon3").click(function(){
-		console.log(this);
+		// console.log("Hi this is working");
 		$(this).toggleClass("open");
 		const smallMenu = $(".smallMenu");
-		// const test = this;
-
-		smallMenu.slideToggle(300);		
-			
+		smallMenu.slideToggle(300);			
 	});
 
+	// When you click on about in small menu, closes the menu (because you're on the about page. Make sure to change if make it sticky.)
+	// Also note: May be problematic in mobile vs when sticky.
 	$("#aboutLink").click(function() {
-
 		$(".smallMenu").slideToggle(300);
 	});
+}
 
+milenaSite.scrolling = () => {
 	function goToByScroll(id) {
 		let toMe = $("." + id)
 		$("html,body").delay(600).animate({ scrollTop: toMe.offset().top }, 1100);
@@ -57,5 +66,8 @@ $(document).ready(function(){
 		goToByScroll("contact");
 		$(".smallMenu").slideToggle(300);
 	});
+}
 
+$(document).ready(function(){	
+	milenaSite.init();
 });
