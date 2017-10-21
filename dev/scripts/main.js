bd332a5c34fe9c2f675793ff5ce756dbff9d0f79
Milena Djokic <milena.djokic@gmail.com>
2017-10-21 12:24:32 -0400

    cleaned up some js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,14 +1,9 @@
 const milenaSite = {}
 
-milenaSite.init = () => {
-	milenaSite.hamburgerMenu();
-	milenaSite.scrolling();
-};
-
 milenaSite.hamburgerMenu = () => {
 	// Hamburger menu in mobile version
 	// When you click on the hamburger icon ("nav-icon3"), opens or closes the menu - with slide motion.
-	$("#nav-icon3").click(function(){
+	$("#nav-icon3").click(() => {
 		// console.log("Hi this is working");
 		$(this).toggleClass("open");
 		const smallMenu = $(".smallMenu");
@@ -17,57 +12,63 @@ milenaSite.hamburgerMenu = () => {
 
 	// When you click on about in small menu, closes the menu (because you're on the about page. Make sure to change if make it sticky.)
 	// Also note: May be problematic in mobile vs when sticky.
-	$("#aboutLink").click(function() {
+	$("#aboutLink").click(() => {
 		$(".smallMenu").slideToggle(300);
 	});
 }
 
 milenaSite.scrolling = () => {
+	// Smooth scroll, not my code. Taken from friend.
 	function goToByScroll(id) {
 		let toMe = $("." + id)
 		$("html,body").delay(600).animate({ scrollTop: toMe.offset().top }, 1100);
 		toMe.addClass("hightlightResult");
-		setTimeout(function () {
+		setTimeout(function() {
 			toMe.removeClass("hightlightResult", 1500);
 		}, 500);
 	};
 
-	$("#navAbout").on("click", function() {
+	$("#navAbout").on("click", () => {
 		goToByScroll("about");
 	});
 
-	$("#mainButton").on("click", function() {
+	$("#mainButton").on("click", () => {
 		goToByScroll("portfolio");
 	});
 
-	$("#navSkills").on("click", function() {
+	$("#navSkills").on("click", () => {
 		goToByScroll("skills");
 	});
 
-	$("#skillsSm").on("click", function() {
+	$("#skillsSm").on("click", () => {
 		goToByScroll("skills");
 		$(".smallMenu").slideToggle(300);
 	});
 
-	$("#navPortfolio").on("click", function() {
+	$("#navPortfolio").on("click", () => {
 		goToByScroll("portfolio");
 	});
 
-	$("#portfolioSm").on("click", function() {
+	$("#portfolioSm").on("click", () => {
 		goToByScroll("portfolio");
 		$(".smallMenu").slideToggle(300);
 	});
 
-	$("#navContact").on("click", function() {
+	$("#navContact").on("click", () => {
 		goToByScroll("contact");
 	});
 
-	$("#contactSm").on("click", function() {
+	$("#contactSm").on("click", () => {
 		goToByScroll("contact");
 		$(".smallMenu").slideToggle(300);
 	});
 }
 
-$(document).ready(function(){	
+milenaSite.init = () => {
+	milenaSite.hamburgerMenu();
+	milenaSite.scrolling();
+};
+
+$(document).ready(() => {	
 	milenaSite.init();
 });
